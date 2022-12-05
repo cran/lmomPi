f210fc3d9a3aadbaf5a57d6b2381e167ce1e0d1e
Emanuele Cordano <emanuele.cordano@gmail.com>
2022-12-05 13:20:04 +0000

    version 0.6.2

--- R/pel.R
+++ R/pel.R
@@ -16,8 +16,14 @@ NULL
 #' @param x vector containg sample. It is utiled to calculete L-moments in case \code{lmom} is set equal to \code{NULL}.
 #' @param indices optional index or tag character vector of the same length of \code{x} used as INDEX for \code{\link{tapply}}. It is used to fit different probability distribution in one sample time series (e. g. months in an year). 
 #' @param spi.scale integer value or \code{NA}. If it is greater than 1, \code{x} is filtered with the sum of a generic element of \code{x} and the previous \code{spi.scale-1} ones (e.g. SPI-3,SPI-6, etc. ). Default is \code{NA} (no filtering) which is equivalent to \code{spi.scale=1}.
+#' @param correction numeric value correction for the 3rd (and higher) L-moment estimation. Default is \code{NULL} , generally it is not used. It is used and suggested to be \code{10^(-10)} in case of a massive function use with \code{lmom=NULL} (e.g. raster cell or zonal statistics). 
 #' @export
 #' @rdname pel 
+#' 
+#' 
+#' @return A numeric vector containing the  parameters of the selected probability distribution. It is a list in case of selection of several probability distributions (i.e. \code{length(distrib)>1}). 
+#'
+#'
 #' @seealso \code{\link{pel...}},\code{\link{pelexp}},\code{\link{pelgam}},\code{\link{pelgev}},\code{\link{pelglo}},\code{\link{pelgpa}},
 #' 
 #' \code{\link{pelgno}},\code{\link{pelgum}},\code{\link{pelkap}},\code{\link{pelln3}},\code{\link{pelnor}},
@@ -46,7 +52,7 @@ NULL
 #'  "nor","pe3","wak","wei")
 #' 
 #'  out_list <- pel(distrib=distrib,lmom=lmom)
-#' 
+#'  \donttest{
 #'  library(rasterList)
 #'  precff <- system.file("map/Mekrou_precipitation.grd", package="rasterList")
 #' 	precs <- stack(precff)
@@ -63,7 +69,7 @@ NULL
 #'  ### example with PEL when x is always 0 or a fixed constant
 #'  xx <- array(0,50)
 #'  out_xx <- pel_x(x=xx,distrib=distrib_prec2)
-#'  
+#'  }
 #' 
 #' 
 #cdfgam 	Gamma distribution
@@ -80,33 +86,35 @@ NULL
 #cdfwei 	Weibull distribution
 
 pel <- function(distrib=c("exp","gam","gev","glo","gpa","gno","gum","kap","ln3","nor","pe3","wak","wei")
-,lmom=NULL,probability_distribution_attrname="probability_distrib",x=NULL,nmom=5, sort.data=TRUE, ratios=sort.data, trim=0,indices=NULL,spi.scale=NA,...) {
-
+,lmom=NULL,probability_distribution_attrname="probability_distrib",x=NULL,nmom=5, sort.data=TRUE, ratios=sort.data, trim=0,indices=NULL,spi.scale=NA,correction=NULL,...) {
+  
+  ##correction_global <<- correction
 	out <- try(stop("Generic Error!!"),silent=TRUE)
 	call <- sys.call()
-	
+	if (is.null(correction)) correction <- NA
 	if (class(x) %in% c("numeric","integer")){
 		
 		## ADDED BY ECOR ON 2017-06-16
 		if (length(spi.scale)<1) spi.scale <- as.numeric(NA)
 		if ((class(spi.scale) %in% c("numeric","integer")) & (!is.na(spi.scale))) {
 		
-			x <- as.vector(filter(x,rep(1,spi.scale[1]),sides=1))
+			x <- as.vector(stats::filter(x,rep(1,spi.scale[1]),sides=1))
 		}
 		
 		## END ADDED BY ECOR ON 2017-06-16
 		
+
 		if (length(indices)==length(x)) {
 			
 			
 				pelxx <- function(x,indices=NULL,...) {
 				
-				pel(x=x,indices=NULL,...)
+				pel(x=x,indices=NULL,...) ## EC 20221016 pel(x=x,indices=NULL,...)
 			}
 				
 				
 			out <- tapply(X=x,FUN=pelxx,INDEX=indices,distrib=distrib
-					,lmom=lmom,probability_distribution_attrname=probability_distribution_attrname,nmom=nmom, sort.data=sort.data, ratios=sort.data, trim=trim,indices=NULL,...) 
+					,lmom=lmom,probability_distribution_attrname=probability_distribution_attrname,nmom=nmom, sort.data=sort.data, ratios=sort.data, trim=trim,indices=NULL,correction=correction,...) 
 				
 			return(out)
 				
@@ -122,7 +130,8 @@ pel <- function(distrib=c("exp","gam","gev","glo","gpa","gno","gum","kap","ln3",
 	
 	if (is.null(lmom)) {
 		
-		lmom <- samlmu(x,nmom=nmom, sort.data=sort.data, ratios=ratios, trim=trim)
+	  
+		lmom <- lmom::samlmu(x,nmom=nmom, sort.data=sort.data, ratios=ratios, trim=trim)
 		
 	}
 	
@@ -152,7 +161,16 @@ pel <- function(distrib=c("exp","gam","gev","glo","gpa","gno","gum","kap","ln3",
 		
 		fundist <- get(paste0("pel",distrib))
 		
-		## EC 20170725
+		
+		
+		
+		if (!is.na(correction) & ratios) {
+		  if (correction<=0.1) correction <- 1-correction  
+		  lmom[-c(1,2)][lmom[-c(1,2)]>correction] <- correction 
+		}
+		##lmom22 <<- lmom
+		if (is.na(lmom[2])) lmom[2] <- 0
+		## END EC 20221025
 		if (lmom[2]==0) {
 			
 			xxqq <- get(paste0("qua",distrib))(runif(10))
@@ -165,6 +183,10 @@ pel <- function(distrib=c("exp","gam","gev","glo","gpa","gno","gum","kap","ln3",
 			cond_no_out <- FALSE
 		}
 		## end EC 20170725
+	
+		##lmom11 <<- lmom
+		
+		### https://search.r-project.org/CRAN/refmans/lmomco/html/are.lmom.valid.html
 		
 		
 		out <- fundist(lmom,...)
